840c7f91648e766937071b79ce8d4fb09303d5d5
Matteo Mirasolo <kiurem66@gmail.com>
2022-05-09 21:15:18 +0200

    added input fields

--- src/main/java/com/web/application/views/charactercreator/CharacterCreatorView.java
+++ src/main/java/com/web/application/views/charactercreator/CharacterCreatorView.java
@@ -2,7 +2,11 @@ package com.web.application.views.charactercreator;
 
 import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.combobox.ComboBox;
+import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextArea;
+import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.server.StreamRegistration;
@@ -12,22 +16,24 @@ import com.vaadin.flow.server.VaadinSession;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import com.lextalionis.*;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Iterator;
+
 import com.lextalionis.Character;
-
-
+import com.lextalionis.*;
 
 
 @PageTitle("CharacterCreator")
 @Route(value = "")
-public class CharacterCreatorView extends VerticalLayout {
+public class CharacterCreatorView extends Div {
 
-    Character c;
+    Character character;
     
     InputStream Filefactory(){
         ByteArrayInputStream bis = null;
         try {
-            bis = new ByteArrayInputStream(XLS.export(c).toByteArray());
+            bis = new ByteArrayInputStream(XLS.export(character).toByteArray());
         } catch (IOException e1) {
             e1.printStackTrace();
         }
@@ -36,9 +42,35 @@ public class CharacterCreatorView extends VerticalLayout {
 
     
     public CharacterCreatorView(){
-        c = new Clan.Assamita();
-        c.setName("Pippo");
+        character = new Clan.Assamita();
         
+        TextField name = new TextField("Nome");
+        name.addValueChangeListener(e -> {
+            character.setName(name.getValue());
+        });
+        add(name);
+
+        ArrayList<String> list = new ArrayList<String>();
+        Iterator<String> it = ClanSelector.iterator();
+        while(it.hasNext()){
+            list.add(it.next());
+        }
+
+        ComboBox<String> clan = new ComboBox<String>("Clan");
+        clan.setItems(list);
+        clan.addValueChangeListener(e -> {
+            //salvataggio dati da tenere
+            String nome = character.getName();
+
+            //cambio pg
+            int v = ClanSelector.get(clan.getValue()); 
+            character = ClanSelector.charSel(v);           
+
+            //aggiornamento valori
+            character.setName(nome);
+        });
+        add(clan);
+
         Button b = new Button("Salva");
         b.addClickListener(e -> {
             StreamResource sr = new StreamResource("Scheda.xlsx", () -> Filefactory());
